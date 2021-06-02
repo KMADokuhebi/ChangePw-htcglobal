3ddf15ab50f308aac95b4f2bcf0a64b1c15a068c
kmadokuhebi <kmadokuhebi@gmail.com>
2021-06-02 11:09:54 +0700

    bo jwt

--- app/webtest/urls.py
+++ app/webtest/urls.py
@@ -15,11 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('loginAD.urls')),
